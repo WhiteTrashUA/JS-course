75743d4cbb7a68e42e4f3199603076a06318667a
Terry <trssmlchk@gmail.com>
2024-02-17 13:48:11 +0200

    Lesson 2

---
+++ Lesson2-main/HomeWork/js/script.js
@@ -0,0 +1,100 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+// console.log(1 > 0);
+// console.log(1 < 0);
+// console.log(0 > 0);
+// console.log(0 < 0);
+// console.log(-3 > 0);
+// console.log(-3 < 0);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+// let a = "test";
+// console.log(a === "test");
+
+// console.log(a === "qwerty");
+
+// console.log(a === "true");
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// let area = 1;
+// let area1 = 10;
+// let area2 = 13;
+
+// if (area2 > 10) {
+//   console.log(area2 - 5);
+// } else {
+//   console.log(area2 + 5);
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const months = prompt("Введіть по рахунку від 1 до 12 місяць");
+// let value;
+// switch (months) {
+//   case "1":
+//     value = "Січень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "2":
+//     value = "Лютий";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "3":
+//     value = "Березень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "4":
+//     value = "Квітень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "5":
+//     value = "Травень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "6":
+//     value = "Червень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "7":
+//     value = "Липень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "8":
+//     value = "Серпень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "9":
+//     value = "Вересень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "10":
+//     value = "Жовтень";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "11":
+//     value = "Листопад";
+//     alert(` ${value} місяць `);
+//     break;
+//   case "12":
+//     value = "Грудень";
+//     alert(` ${value} місяць `);
+//     break;
+//   default:
+//     alert(`в році всього 12 місяців, введіть коректне число`);
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+// let value = prompt("Введіть трьохзначне число");
+
+// if (value > 99 && value < 1000) {
+//   alert(`Вірно - ${Number(value[0]) + Number(value[1]) + Number(value[2])}`);
+//   // alert(`Вірно - ${sumValue}`);
+// } else {
+//   alert(`Вdедіть коректне число`);
+// }
