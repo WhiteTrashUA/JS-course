c6a959cdcdd2ed53704bffea25ae7ea71c19d52f
Terry <trssmlchk@gmail.com>
2024-03-02 11:51:13 +0200

    Lesson 9 withour 3task

---
+++ Lesson9-main/HomeWork/js/script.js
@@ -0,0 +1,39 @@
+// const nav = document.querySelector(".nav");
+
+// nav.addEventListener("click", handleClick);
+
+// function handleClick(event) {
+//   const checkClass = nav.querySelector("li.active");
+
+//   if (checkClass) {
+//     event.target.classList.remove("active");
+//   } else {
+//     event.target.classList.add("active");
+//   }
+// }
+
+// 1 //
+
+// const btn1 = document.querySelector(".btn1");
+// btn1.addEventListener("click", hideText);
+// function hideText(event) {
+//   const textDiv = document.querySelector("#text");
+//   textDiv.style.display = "none";
+// }
+
+// 2 //
+// const btn1 = document.querySelector(".btn1");
+
+// btn1.addEventListener("click", cluck);
+
+// function cluck(event) {
+//   const checkClass = document.querySelector("active");
+
+//   if (checkClass) {
+//     event.target.classList.remove("active");
+//   } else {
+//     event.target.classList.add("active");
+//   }
+// }
+
+// 3 //
