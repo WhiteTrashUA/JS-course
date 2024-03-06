f102b578a4cdb35b212f31a3eb1fc2778266addd
Terry <trssmlchk@gmail.com>
2024-03-06 16:37:20 +0200

    Lesson 11

---
+++ Lesson11-main/index.js
@@ -0,0 +1,14 @@
+// import { user } from "./module";
+// import "./style.css";
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
+
+// // console.log("you name: " + user);
+
+// console.log(isEmail("foo@bar.com"));
+// console.log(isURL("foo@bar.com"));
+// console.log(toDate("11.12.2000"));
+
+// var moment = require("moment");
+// console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
